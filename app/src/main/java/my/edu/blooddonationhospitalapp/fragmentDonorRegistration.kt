e9fbb2c7d0db6fb622089fa6b8d0e92936263335
Ruthressh21 <ruthresshvikneswaran@gmail.com>
2022-08-23 20:27:49 +0800

    initial commit-ruth

--- app/src/main/java/my/edu/blooddonationhospitalapp/fragmentDonorRegistration.kt
+++ app/src/main/java/my/edu/blooddonationhospitalapp/fragmentDonorRegistration.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,16 +15,19 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [registerFragment.newInstance] factory method to
+ * Use the [fragmentDonorRegistration.newInstance] factory method to
  * create an instance of this fragment.
  */
-class registerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+class fragmentDonorRegistration : Fragment() {
 
+    lateinit var bloodGroupOption: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+
 
         }
     }
 
+}
---
+++ app/src/main/java/my/edu/blooddonationhospitalapp/framentDonorLogin.kt
@@ -0,0 +1,15 @@
+package my.edu.blooddonationhospitalapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class framentDonorLogin : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
